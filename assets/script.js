c645350a0857ff3decfed7f8123f312fb3903ffe
Xavier <xavier3435@icloud.com>
2024-07-16 15:02:29 -0400

    materialize. Jquery

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,17 @@
-var()
+// Initialize Materialize components
+$(document).ready(function(){
+    $('.modal').modal();
+    $('.sidenav').sidenav();
+    $('.scrollspy').scrollSpy();
+    
+    // Smooth scrolling for anchor links
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+});
