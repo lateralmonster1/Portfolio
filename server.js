f1e6a977b45fd6aa9f1a020507f56044891da062
Xavier <xavier3435@icloud.com>
2024-08-02 11:26:04 -0400

    file moved

--- server.js
+++ server.js
@@ -5,19 +5,19 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3002;
 
-// Middleware to parse incoming form data
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
 
-// Route to serve the index.html file for the root URL
+app.use(express.static(path.join(__dirname, 'assets')));
+
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'assets', 'index.html'));
 });
 
-// Route to handle form submissions
+
 app.post('/submit-form', (req, res) => {
     const { name, email, message } = req.body;
     console.log('Form submission received:');
@@ -27,7 +27,7 @@ app.post('/submit-form', (req, res) => {
     res.send('Form submission received!');
 });
 
-// Start the server
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
